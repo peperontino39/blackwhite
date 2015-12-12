1835fef4794398bb64cfc65102d42de0664c208b
Daiki Hirasawa <flat053117117@gmail.com>
2015-12-12 18:06:55 +0900

    ゲームパッド更新
    星名が１０分でやってくれました。

--- whiteblack/Top/GamePad.h
+++ whiteblack/Top/GamePad.h
@@ -1,8 +1,67 @@
 #pragma once
-#include "include.h"
 
+#include "Top.h" 
 
 //////大ちゃん
 /////ここは大ちゃんの好きに使っていいよ
 //////最終的にはGamePad::PushBoton('1')とかになって
 //分かりやすくなってればいいよ
+
+class Pad_Base
+{
+public:
+	Pad_Base()
+		: gamepad_num(env.numGamePad())
+		, pad(env.gamePad(0))
+	{
+
+	}
+	bool isPressButton(const int button)
+	{
+		if (gamepad_num)
+		{
+			if (pad.isPressButton(button))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool isPushButton(const int button)
+	{
+		if (gamepad_num)
+		{
+			if (pad.isPushButton(button))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+private:
+	size_t gamepad_num;
+	GamePad& pad;
+};
+
+class Pad{
+public:
+	static Pad_Base& Get()
+	{
+		static Pad_Base pad;
+		return pad;
+	}
+};
